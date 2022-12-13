4744f7dd30ab5ad631ceb3b6d26b02403444ee40
Thalisson <thalisson.ad@gmail.com>
2022-12-13 18:51:44 -0300

    otimização

---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { TableService } from './components/table-products.service';
+
+export interface ProductData {
+  id: string;
+  title: string;
+  price: string;
+  brand: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'front-digifred';
+  displayedColumn: string[] = ['id', 'title', 'price', 'brand'];
+  dataSource!: MatTableDataSource<ProductData>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  posts: any;
+  error: any;
+
+  constructor(private service: TableService) {
+    this.service.getData().subscribe({
+      next: (data: any) => {
+        console.log(data.products);
+        this.posts = data.products;
+
+        this.dataSource = new MatTableDataSource(this.posts);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      }
+      // ,
+      // (error: any) => {
+      //   this.error = error;
+      //   console.log('ERRO: ', error);
+      // }
+  });
+  }
+
+  applyFilter(event: Event) {
+    const filtrarValor = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filtrarValor.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+})
+export class AppModule {}
